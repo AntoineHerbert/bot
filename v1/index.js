2b6646f00725421bd971591f3e60a05966fa4a04
antoine bebert <antoine.dralaes@gmail.com>
2022-06-27 04:34:48 +0200

    try hide token

--- v1/index.js
+++ v1/index.js
@@ -6,7 +6,7 @@ module.exports = (client, message) => {
 }
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
-const token = 'OTkwNzU4MzE3ODgwMTM5ODU3.GVyVfp.ZVA1lx4Z1FBOtzgg2pljczET8u_x-n8p7AENKw'
+const token = 'OTkwNzU4MzE3ODgwMTM5ODU3.GlG7sT.uzhmKNatV6DQI--EUgTl8ANXO2PCEY1rFTmaTg'
 client.once('ready', () => {
    console.log('RDY');
 });
